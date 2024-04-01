caad0c48a286fa606f756e1bfa09d00e4e786623
luchaoxin <chaoxin.lu@pistonint.com>
2024-04-01 11:58:40 +0800

    升级版本号

--- build-imaga.sh
+++ build-imaga.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-version=1.0.8
-image_name=xyz.com/library/container/init-container-s3:1.0.8
-docker buildx build --platform linux/amd64,linux/arm64 -f DockerfileOrig -t "${image_name}:${version}" . #--push
-docker buildx build --platform linux/amd64,linux/arm64 -f DockerfileOrig -t "${image_name}:latest" . #--push
+version=1.0.15
+#image_name=xin8/init-container-s3
+image_name=gzv-reg.piston.ink/library/container/init-container-s3
+docker buildx build --platform linux/amd64,linux/arm64 -f DockerfileOrig -t "${image_name}:${version}" . --push
+docker buildx build --platform linux/amd64,linux/arm64 -f DockerfileOrig -t "${image_name}:latest" . --push
--- go.mod
+++ go.mod
@@ -1,7 +1,7 @@
 module init-container-s3
 
-go 1.21.0
+go 1.22.1
 
-require github.com/aws/aws-sdk-go v1.49.9
+require github.com/aws/aws-sdk-go v1.51.11
 
 require github.com/jmespath/go-jmespath v0.4.0 // indirect
